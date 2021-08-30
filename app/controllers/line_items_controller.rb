5c4dfe67785de564cea23c2406947c4ee51205ff
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-08-30 09:20:08 +0200

    Added ActionCable For products

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,8 +29,10 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_index_url }
+        format.js  { @current_item = @line_item }
+        format.json { render :show, 
+          status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', '1 \u00D7 Programming Ruby 1.9'
+    assert_select 'td', 'Programming Ruby 1.9'
   end
 
   test "should show line_item" do
@@ -49,4 +49,19 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+      xhr: true
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
+
+
+
 end
